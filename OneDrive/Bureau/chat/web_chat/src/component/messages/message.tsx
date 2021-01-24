39d1a3e432cb8e3fcb2151f0ed6772fea821df89
hakimmaaouia <hakimmaaouia@gmail.com>
2021-01-24 21:46:38 +0100

    fix responsive && add animation

--- OneDrive/Bureau/chat/web_chat/src/component/messages/message.tsx
+++ OneDrive/Bureau/chat/web_chat/src/component/messages/message.tsx
@@ -3,81 +3,79 @@ import "./message.css";
 import Avatar from "@material-ui/core/Avatar";
 import Emoji from "react-emoji-render";
 
-const Adminmessage=({
+const Adminmessage = ({
   message: {
     Name,
     message,
     dates: { date, date_ob, hours, minutes, month, seconds, year },
   },
-}: any)=>{
-  return(
-    <div className="message-data-time centre">
-      {message}  {hours}:{minutes}:{seconds} {year}/{month}/{date}
-      </div>
-  )
-}
-
-
-
-const Usermessage=({
-  message: {
-    Name,
-    message,
-    dates: { date, date_ob, hours, minutes, month, seconds, year },
-  },
-}: any)=>{
-return(
-  <div className="containerMessage">
-  <div style={{ display: "flex"}}>
-    <Avatar>{Name[0].toUpperCase()}</Avatar>
-    <div className="data_name">
-      <span>{Name}</span>{" "}
-      <span className="message-data-time">
-        {hours}:{minutes}:{seconds} {year}/{month}/{date}
-      </span>
-    </div>
-  </div>
-  <div className="message-box">
-  <Emoji text={message} />
-  </div>
-</div>
-)
-}
-
-const Mymessage=({
-  message: {
-    Name,
-    message,
-    dates: { date, date_ob, hours, minutes, month, seconds, year },
-  },
-}: any)=>{
-return(
-<div className="my-message-container" >
-<div className="my-message-descreption">
-<div className="data_name">
-      <span className="message-data-time">
-        {hours}:{minutes}:{seconds} {year}/{month}/{date}
-      </span>
-      <span>  {Name}  </span>{" "}
-    </div>
-     <Avatar>{Name[0].toUpperCase()}</Avatar>
-</div>
-  <div className="my-message-box">
-{message}
-  </div>
-</div>
-
-)
-}
-
-
-
-const Message = ({message,Name}: any) => {
-  console.log(Name)
+}: any) => {
   return (
-   <div>
-    {message.Name==="admin"?<Adminmessage message={message}/>:(Name===message.Name)?<Mymessage message={message}/>:<Usermessage message={message}/>}
-     </div>
+    <div className="message-data-time centre">
+      {message} {hours}:{minutes}:{seconds} {year}/{month}/{date}
+    </div>
+  );
+};
+
+const Usermessage = ({
+  message: {
+    Name,
+    message,
+    dates: { date, date_ob, hours, minutes, month, seconds, year },
+  },
+}: any) => {
+  return (
+    <div className="containerMessage">
+      <div style={{ display: "flex" }}>
+        <Avatar>{Name[0].toUpperCase()}</Avatar>
+        <div className="data_name">
+          <span>{Name}</span>{" "}
+          <span className="message-data-time">
+            {hours}:{minutes}:{seconds} {year}/{month}/{date}
+          </span>
+        </div>
+      </div>
+      <div className="message-box">
+        <Emoji text={message} />
+      </div>
+    </div>
+  );
+};
+
+const Mymessage = ({
+  message: {
+    Name,
+    message,
+    dates: { date, date_ob, hours, minutes, month, seconds, year },
+  },
+}: any) => {
+  return (
+    <div className="my-message-container">
+      <div className="my-message-descreption">
+        <div className="data_name">
+          <span className="message-data-time">
+            {hours}:{minutes}:{seconds} {year}/{month}/{date}
+          </span>
+          <span> {Name} </span>{" "}
+        </div>
+        <Avatar>{Name[0].toUpperCase()}</Avatar>
+      </div>
+      <div className="my-message-box">{message}</div>
+    </div>
+  );
+};
+
+const Message = ({ message, Name }: any) => {
+  return (
+    <div>
+      {message.Name === "admin" ? (
+        <Adminmessage message={message} />
+      ) : Name === message.Name ? (
+        <Mymessage message={message} />
+      ) : (
+        <Usermessage message={message} />
+      )}
+    </div>
   );
 };
 export default Message;
--- OneDrive/Bureau/chat/web_chat/src/component/messages/messages.tsx
+++ OneDrive/Bureau/chat/web_chat/src/component/messages/messages.tsx
@@ -1,20 +1,33 @@
-import React, { useState, useEffect } from "react";
-import  Message from "./message"
-import "./message.css"
-
-const Messages=({socket,Name}:any)=>{
-    
-    const [messages, setmessages] = useState<any[]>([]);
-    useEffect(() => {
+import React, { useState, useEffect ,useRef } from "react";
+import Message from "./message";
+import "./message.css";
+const Messages = ({ socket, Name }: any) => {
+  
+  const [messages, setmessages] = useState<any[]>([]);
+  useEffect(() => {
     socket?.on("messages", (message: any) => {
-        setmessages(messages => [...messages, message]);
-      })
-    },[])
+      setmessages((messages) => [...messages, message]);
+      if(messageEl.current){
+     let scroll=messageEl.current.scrollHeight-messageEl.current.clientHeight
+     messageEl.current.scrollTo(0, scroll);
+        
+      }
+  
+    });
+  }, []);
+  const messageEl = useRef<HTMLInputElement>(null);
+  
+ 
+
+
+
+  return (
+    <div className="chatbox" ref={messageEl} >
+      {messages.map((message: any, i: any) => (
+        <Message key={i} message={message} Name={Name}  />
+      ))}
     
-    return(
-        <div className="chatbox"> 
-        {messages.map((message:any, i:any) =><Message  key={i} message={message} Name={Name}/>)}
-        </div>
-    )
-}
+    </div>
+  );
+};
 export default Messages;
